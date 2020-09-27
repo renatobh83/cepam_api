08073d9741ea02b73dc3aa4a9229ea1e871a3d53
Renatobh83 <neo_rlm@hotmail.com>
2020-09-26 21:40:00 -0300

    changeRouteUser

--- src/routes/usersRouter.js
+++ src/routes/usersRouter.js
@@ -2,14 +2,14 @@ const users = require("express").Router();
 const usersController = require("../app/controllers/usersController");
 
 // login
-users.get("/api/users/login", usersController.loginUser);
+users.get("/api/usuarios/login", usersController.loginUser);
 
 // rotas usuarios
-users.get("/api/users", usersController.indexUsers);
-users.get("/api/users/:email", usersController.findUser);
-users.post("/api/users", usersController.store);
-users.put("/api/users/:email", usersController.UpdateUserPatient);
-users.delete("/api/users/:email", usersController.deactiveOrActive);
+users.get("/api/usuarios", usersController.indexUsers);
+users.get("/api/usuarios/:email", usersController.findUser);
+users.post("/api/usuarios", usersController.store);
+users.put("/api/usuarios/:email", usersController.UpdateUserPatient);
+users.delete("/api/usuarios/:email", usersController.deactiveOrActive);
 
 // rotas pacientes
 users.get("/api/pacientes/", usersController.indexPacientes);
