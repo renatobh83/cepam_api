3bf7b68e4f14a2555a736b2d411dc56a286c6726
Renato <neo_rlm@hotmail.com>
2020-10-07 14:59:46 -0300

    acesso

---
+++ src/middlewares/permissao.js
@@ -0,0 +1,36 @@
+const User = require("../app/models/users");
+const Grupos = require("../app/models/grupos");
+const Permissao = require("../app/models/permissoes");
+
+module.exports = {
+  check: function (req, res, next) {
+    console.log(req.user);
+
+    User.findOne({ email: req.user.email }).then(async (response) => {
+      if (!response.paciente) {
+        const idPermissao = await Permissao.findOne(
+          { nome: req.path.split("/")[2].toUpperCase() },
+          { _id: 1 }
+        );
+        Grupo;
+        const grupo = await Grupos.findOne(
+          { _id: response.grupoId },
+          { permissaoId: 1, _id: 0 }
+        );
+        if (
+          grupo.permissaoId.includes(idPermissao !== null ? idPermissao._id : 0)
+        ) {
+          return next();
+        } else {
+          var err = new Error();
+          err.status = 401;
+          next(err);
+        }
+      } else {
+        var err = new Error();
+        err.status = 401;
+        next(err);
+      }
+    });
+  },
+};
--- src/routes/usersRouter.js
+++ src/routes/usersRouter.js
@@ -1,19 +1,20 @@
-const users = require('express').Router();
+const users = require("express").Router();
 
-const usersController = require('../app/controllers/usersController');
-const verifyToken = require('../middlewares/verifyToken');
+const usersController = require("../app/controllers/usersController");
+const { check } = require("../middlewares/permissao");
+const verifyToken = require("../middlewares/verifyToken");
 
 // login
-users.get('/api/usuarios/login', verifyToken, usersController.loginUser);
+users.get("/api/usuarios/login", verifyToken, usersController.loginUser);
 
 // rotas usuarios
-users.get('/api/usuarios', usersController.indexUsers);
-users.get('/api/usuarios/:email', usersController.findUser);
-users.post('/api/usuarios', usersController.store);
-users.put('/api/usuarios/:email', usersController.UpdateUserPatient);
-users.delete('/api/usuarios/:email', usersController.deactiveOrActive);
+users.get("/api/usuarios", verifyToken, check, usersController.indexUsers);
+users.get("/api/usuarios/:email", usersController.findUser);
+users.post("/api/usuarios", usersController.store);
+users.put("/api/usuarios/:email", usersController.UpdateUserPatient);
+users.delete("/api/usuarios/:email", usersController.deactiveOrActive);
 
 // rotas pacientes
-users.get('/api/pacientes/', usersController.indexPacientes);
+users.get("/api/pacientes/", usersController.indexPacientes);
 
 module.exports = users;
