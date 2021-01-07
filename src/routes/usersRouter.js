87ef9906e01ac4d818b984d7feb417682d763a2a
renatobh83 <neo_rlm@hotmail.com>
2021-01-07 12:58:10 -0300

    ProtectRout

--- src/routes/usersRouter.js
+++ src/routes/usersRouter.js
@@ -8,14 +8,15 @@ const verifyToken = require('../middlewares/verifyToken');
 users.get('/api/usuarios/login', verifyToken, usersController.loginUser);
 
 // rotas usuarios
-users.get('/api/usuarios', verifyToken, usersController.indexUsers);
+users.get('/api/usuarios', verifyToken,check, usersController.indexUsers);
 // users.get('/api/usuarios/:email', verifyToken, usersController.findUser);
-users.post('/api/usuarios', verifyToken, usersController.store);
-users.post('/api/paciente', verifyToken, usersController.storePaciente);
-users.put('/api/usuarios/:_id', verifyToken, usersController.UpdateUserPatient);
+users.post('/api/usuarios', verifyToken,check, usersController.store);
+users.post('/api/paciente', verifyToken,check, usersController.storePaciente);
+users.put('/api/usuarios/:_id', verifyToken,check, usersController.UpdateUserPatient);
 users.delete(
   '/api/usuarios/:email',
   verifyToken,
+  check,
   usersController.deactiveOrActive
 );
 
